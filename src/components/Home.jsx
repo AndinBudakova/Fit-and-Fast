c5d5c55f7d3a41674d99f0d1defb73e71db94dc5
Andin <ab51129@ubt-uni.net>
2023-05-14 22:44:37 +0200

    Home page
    rregullimi i faqes, shtimi i teksteve, butonave .... fixing it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './styles/Home.css'
 import Header from './Header/Header'
+import Heart from "../../src/assets/heart.jpg"
+
 const Home = () => {
     return(
         <div className="home">
@@ -12,7 +14,7 @@ const Home = () => {
                     <span>Inspiration does exist, but it must find you working</span>
                 </div>
 
-                <div className="hero-text">
+                <div className="home-text">
                     <div><span className='stroke-text'>You should never stay</span>
                     </div>
                     <div><span>At the same level</span>
@@ -25,9 +27,34 @@ const Home = () => {
                     </div>
 
                 </div>
-                
+
+                <div className="figures">
+                    <div>
+                        <span>+110</span>
+                        <span>expert coachs</span>
+                    </div>
+                    <div>
+                        <span>+800</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>                 
+                </div>
+                <div className="home-buttons">
+                        <buttons className="btn">Start</buttons>
+                        <buttons className="btn">Learn More</buttons>
+                    </div> 
             </div>
-            <div className="right-h">right side</div>
+            <div className="right-h">
+                <button className="btn">Join Now</button>
+
+                <div className="heart-rate">
+                    <img src={Heart} alt=""/>
+                    <span>Heart Rate</span><span>116 bpm</span>
+                </div>
+            </div>
         </div>
     )
 }
